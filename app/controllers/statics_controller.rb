4d36ceaf71622e950e07fa7957a69de70dc7e3ad
ekenecf <ekens2u@gmail.com>
2022-09-13 16:16:40 +0100

    Fix rubocop offences

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,3 @@
 class StaticsController < ApplicationController
-
-  def index
-  end
+  def index; end
 end
--- test/system/statics_test.rb
+++ test/system/statics_test.rb
@@ -1,39 +1,39 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class StaticsTest < ApplicationSystemTestCase
   setup do
     @static = statics(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit statics_url
-    assert_selector "h1", text: "Statics"
+    assert_selector 'h1', text: 'Statics'
   end
 
-  test "should create static" do
+  test 'should create static' do
     visit statics_url
-    click_on "New static"
+    click_on 'New static'
 
-    click_on "Create Static"
+    click_on 'Create Static'
 
-    assert_text "Static was successfully created"
-    click_on "Back"
+    assert_text 'Static was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Static" do
+  test 'should update Static' do
     visit static_url(@static)
-    click_on "Edit this static", match: :first
+    click_on 'Edit this static', match: :first
 
-    click_on "Update Static"
+    click_on 'Update Static'
 
-    assert_text "Static was successfully updated"
-    click_on "Back"
+    assert_text 'Static was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Static" do
+  test 'should destroy Static' do
     visit static_url(@static)
-    click_on "Destroy this static", match: :first
+    click_on 'Destroy this static', match: :first
 
-    assert_text "Static was successfully destroyed"
+    assert_text 'Static was successfully destroyed'
   end
 end
